e2b2d1e827aa2f7a305826c55fd017ece9c14cba
Alex Hill <alex.c.hill@outlook.com>
2022-03-29 20:48:29 -0600

    working battery monitor with 3s Lipo

--- src/BatteryMonitor/BatteryMonitor.ino
+++ src/BatteryMonitor/BatteryMonitor.ino
@@ -1,13 +1,13 @@
 const int BATT_MEAS_PIN = 0;
 
-const float MAX_MEAS_VOLT = 5.0; // max voltage arduino analog input will handle
+const float MAX_MEAS_VOLT = 5;   // max voltage arduino analog input will handle
 const int AIN_ADC_BITS = 10;     // analog number analog/digital convert number of bits
 const float AIN_VOLT_PER_UNIT = MAX_MEAS_VOLT / pow(2, AIN_ADC_BITS);
 
-const float BATT_VOLT_MAX = 7;
+const float BATT_VOLT_MAX = 12;
 
-const float BATT_MEAS_R1 = 9.89e3;
-const float BATT_MEAS_R2 = 9.83e3;
+const float BATT_MEAS_R1 = 2 * 9.89e3;
+const float BATT_MEAS_R2 = 9.85e3;
 const float BATT_MEAS_RA = 100e6;
 
 // total resistance of resistor 2 and analog input impedance in parallel
@@ -17,7 +17,7 @@ const float BATT_MEAS_MAX_CURRENT = BATT_VOLT_MAX / (BATT_MEAS_R1 + BATT_MEAS_R2
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial)
     ;
 
